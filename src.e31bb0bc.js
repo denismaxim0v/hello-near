466bd1391b9380284f3755f502afa169361e39d6
denismaxim0v <denis_maxim0v@protonmail.com>
2022-04-13 21:54:52 +0300

    Updates

--- src.e31bb0bc.js
+++ src.e31bb0bc.js
@@ -17326,7 +17326,7 @@ __exportStar(require("./browser-connect"), exports);
 require("error-polyfill");
 
 },{"./key_stores/browser-index":"../node_modules/near-api-js/lib/key_stores/browser-index.js","./common-index":"../node_modules/near-api-js/lib/common-index.js","./browser-connect":"../node_modules/near-api-js/lib/browser-connect.js","error-polyfill":"../node_modules/error-polyfill/index.js"}],"config.js":[function(require,module,exports) {
-var CONTRACT_NAME = "dev-1649875423169-11155276131456" || 'denismaxim0v.testnet';
+var CONTRACT_NAME = 'hello-world.denismaxim0v.testnet';
 
 function getConfig(env) {
   switch (env) {
@@ -17611,7 +17611,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "45247" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "43319" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
